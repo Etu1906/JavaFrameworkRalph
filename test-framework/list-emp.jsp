78eafa5488e7819d3e09662782b8d8328207291c
ralph <yoanrazafinjaka@gmail.com>
2023-04-04 11:32:09 +0300

    sprint6

--- test-framework/list-emp.jsp
+++ test-framework/list-emp.jsp
@@ -1,3 +1,14 @@
+<%@ page import = "model.haha.Emp"%>
+<%@ page import = "java.util.Vector"%>
+<% 
+    Vector<Emp> list = (Vector<Emp>) request.getAttribute("lst");
+%>
 <h1>
     Welcome emp
 </h1>
+
+<%
+    for( int i = 0 ; i != list.size() ; i++ ){
+        out.print(  list.get(i).getNom()+"</br>" );
+    }
+%>
--- test-framework/src/Emp.java
+++ test-framework/src/Emp.java
@@ -1,14 +1,51 @@
 package model.haha;
 
+import java.util.List;
+import java.util.Vector;
+
 import etu1906.framework.view.*;
 import model.*;
 
 public class Emp {
 
+    String nom;
+
+    public Emp(){
+
+    }
+
+    public Emp(String nom) {
+        this.nom = nom;
+    }
+
+    public static Vector<Emp> listEmp(){
+        Vector<Emp> list = new Vector<Emp>();
+
+        Emp emp1 = new Emp("Jean");
+        Emp emp2 = new Emp("Paul");
+        Emp emp3 = new Emp("II");
+
+        list.add(emp1);
+        list.add(emp2);
+        list.add(emp3);
+
+        return list;
+    }
+
     @Urls( url="emp-all" )
     public ModelView findAll(){
         System.out.println(" bonjuour ");
         ModelView view = new ModelView( "list-emp.jsp" );
+        Vector<Emp> l = listEmp();
+        view.addItem("lst", l);
         return view;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
     }     
 }
